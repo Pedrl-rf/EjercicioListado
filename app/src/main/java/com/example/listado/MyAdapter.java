2d7c8c73e7d8a308afddb00634acc368f7105b1d
Pedrl-rf <pedrl.rf2001@gmail.com>
2021-02-23 12:43:49 +0100

    23/02/2021 adapter mirar profesor

--- app/src/main/java/com/example/listado/MyAdapter.java
+++ app/src/main/java/com/example/listado/MyAdapter.java
@@ -1,4 +1,36 @@
 package com.example.listado;
 
-public class MyAdapter {
+import android.content.Context;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private Context context;
+    private List<compra> listado;
+
+    public MyAdapter(Context context, List<compra> listado) {
+        this.context = context;
+        this.listado = listado;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
